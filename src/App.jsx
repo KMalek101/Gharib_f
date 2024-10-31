2f167d04d4e6336bc534aa7cd2f50747c5d8dcb1
KMalek101 <kaoucheaneslive@gmail.com>
2024-10-31 18:10:10 +0100

    added the userProfile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import SideBar from "./Components/common/sidebar/Sidebar.jsx"
 import Header from "./Components/common/header/Header.jsx";
 import MainLayout from "./Components/layouts/main/MainLayout.jsx";
+import UserProfile from "./Components/pages/userProfile/UserProfile.jsx";
 
 function App() {
   return<>
+  <div className="container">
+    {/* <UserProfile /> */}
     <MainLayout />
-    {/* <SideBar /> */}
+  </div>
+    
   </>
 }
 
--- src/Components/layouts/main/MainLayout.jsx
+++ src/Components/layouts/main/MainLayout.jsx
@@ -1,12 +1,15 @@
 import Header from "../../common/header/Header";
 import SideBar from "../../common/sidebar/Sidebar"
 import styles from "./MainLayout.module.css"
-
+import UserProfile from "../../pages/userProfile/UserProfile";
 function MainLayout() {
     return<>
         <div className="real">
             <Header />
-            <SideBar />
+            <div className={styles.sidebarANDpage}>
+                <SideBar />
+                <UserProfile />
+            </div>
         </div>
     </>
 }
