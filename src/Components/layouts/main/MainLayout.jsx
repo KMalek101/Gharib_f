e4bc76b94b267114c79a1044dd3fbbc616ff6697
KMalek101 <kaoucheaneslive@gmail.com>
2024-11-01 16:46:49 +0100

    api set up

--- src/Components/layouts/main/MainLayout.jsx
+++ src/Components/layouts/main/MainLayout.jsx
@@ -2,8 +2,22 @@ import Header from "../../common/header/Header";
 import SideBar from "../../common/sidebar/Sidebar"
 import styles from "./MainLayout.module.css"
 import UserProfile from "../../pages/userProfile/UserProfile";
-
+import axios from "axios"
+import { useEffect, useState } from "react";
 function MainLayout() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/retreive/1/')
+          .then((response) => {
+            setData(response.data);
+          })
+          .catch((error) => {
+            console.error("There was an error fetching data!", error);
+          });
+    }, []);
+
+    console.log("the data is : ", data);
     return<>
         <div className="real">
             <Header />
